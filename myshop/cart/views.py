56c22aea9cc171d9ed52956175b184a1d38f9061
Kiarash Shahedi <puddin@kali>
2024-08-16 12:51:52 +0330

    fixing adding/updating products number in product detail page

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -2,26 +2,33 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cart, CartItem, Product
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.db.models import Sum
 
 @login_required
 def cart_detail(request):
     cart, created = Cart.objects.get_or_create(user=request.user)
-    return render(request, 'cart/cart_detail.html', {'cart': cart})
+    # number of products in cart 
+    cart_items_count = cart.items.aggregate(Sum('quantity'))['quantity__sum'] or 0
+
+    return render(request, 'cart/cart_detail.html', {'cart': cart, 'cart_items_count': cart_items_count})
 
 @login_required
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     cart, created = Cart.objects.get_or_create(user=request.user)
     
+    # Get the quantity from the POST data
+    quantity = int(request.POST.get('quantity', 1))
+    
+    # Get or create a CartItem for the product
     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
     
-    if created:
-        cart_item.quantity = 1  # Default quantity if newly added
-    else:
-        # Ensure that the quantity does not exceed the maximum limit
-        if cart_item.quantity < 5:
-            cart_item.quantity += 1
+    # Update the quantity directly, don't add to the existing quantity
+    if quantity > 0:
+        cart_item.quantity = min(quantity, 5)  # Ensure it doesn't exceed the maximum limit (e.g., 5)
         cart_item.save()
+    else:
+        cart_item.delete()  # Optionally, delete the item if quantity is set to 0
     
     return redirect('cart_detail')
 
